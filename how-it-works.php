fd74685a6eace2e060bf85a948a7fc9be3c9dacb
jvu123 <37727359+jvu123@users.noreply.github.com>
2022-06-26 13:22:20 -0400

    added html content into php files

--- how-it-works.php
+++ how-it-works.php
@@ -15,6 +15,103 @@
     <section class="hiw-intro">
         <?php include('templates/nav.php'); ?>
 
+        <div class="hiw-intro-content">
+            <h1>How it works</h1>
+
+            <img src="assets/molecules-media-video-1-copy@3x.webp" alt="video-thumbnail">
+
+        </div>
+
+    </section>
+
+    <section class="speed">
+        <img src="assets/ipad@3x.webp" alt="ipad">
+
+        <div class="speed-content">
+            <h2>Speed up your design process</h2>
+
+            <div class="speed-row">
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+
+            <div class="speed-row">
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+
+            <div class="speed-row">
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+
+                <div class="speed-col">
+                    <img src="" alt="">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="last-kit">
+        <div class="last-kit-content">
+            <h2>The last wireframe kit you'll ever need</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore 
+                magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            <ul>
+                <img src="" alt="">
+                <li>Modern and intuituve design</li>
+                <img src="" alt="">
+                <li>Full customizable components</li>
+                <img src="" alt="">
+                <li>Detailed layouts & elements</li>
+                <img src="" alt="">
+                <li>Design pattern best pratice</li>
+            </ul>
+        </div>
+
+        <img src="" alt="">
+    </section>
+
+    <section class="signup">
+        <div class="signup-content">
+            <h3>Get the most out of Form</h3> <!--needs to be modified specifically --->
+
+            <p></p>
+        </div>
+
+        <div class="signup-btn">
+            <a href="">SIGNUP NOW</a>
+
+            <a href="">READ MORE</a>
+        </div>
+
     </section>
 
     <section class="hiw-footer">
